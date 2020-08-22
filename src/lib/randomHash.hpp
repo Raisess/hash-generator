625bbd18baa8c0acd3e7237b773a26a8a300c763
danillu <48914278+Raisess@users.noreply.github.com>
2020-08-22 12:13:45 -0300

    created a debug mode in randomHash gen

--- src/lib/randomHash.hpp
+++ src/lib/randomHash.hpp
@@ -1,10 +1,21 @@
+#include <iostream>
 #include <caracs.hpp>
 
-auto randomHash(int len, bool symbols) {
+using namespace std;
+
+auto randomHash(int len, bool symbols, bool debug) {
 	char* hash_[len];
 
 	for (int i = 0; i < len; i++) {
+		if (debug) {
+			cout << caracs[i] << "\n";
+		}
+
 		hash_[i] = caracs[i];
+	}
+
+	if (debug) {
+		cout << hash_ << "\n";
 	}
 
 	return hash_;
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 int main() {
-	const int leng      = 10;
-	char** myHash[leng] = { randomHash(leng, false) };
+	const  int leng     = 26;
+	char** myHash[leng] = { randomHash(leng, false, true) };
 
-	cout << myHash << "\n";
+	for (int i = 0; i < leng; i++) {
+		cout << myHash[i];
+	}
+
+	cout << "\n";
 
 	system("pause");
 	return 0;
