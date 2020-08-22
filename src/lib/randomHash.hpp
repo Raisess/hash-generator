55b6b33ce7b08c0f3bb0aa64a3a6d3ac8e3089b3
danillu <48914278+Raisess@users.noreply.github.com>
2020-08-22 12:21:29 -0300

    updated randomHash function debug

--- src/lib/randomHash.hpp
+++ src/lib/randomHash.hpp
@@ -7,11 +7,12 @@ auto randomHash(int len, bool symbols, bool debug) {
 	char* hash_[len];
 
 	for (int i = 0; i < len; i++) {
-		if (debug) {
-			cout << caracs[i] << "\n";
-		}
-
 		hash_[i] = caracs[i];
+
+		if (debug) {
+			cout << caracs[i];
+			cout << hash_[i];
+		}
 	}
 
 	if (debug) {
