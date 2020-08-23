631f21242a07b3e8ba1a1273027c29795e155319
danillu <48914278+Raisess@users.noreply.github.com>
2020-08-23 10:19:35 -0300

    added config options to '#define' values

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,14 @@
 #include <randomHash.hpp>
 
 #define LENG 40
+#define SYMB false
+#define DEBU true
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main() {
 	// generated random hash array
-	char** myHash = randomHash(LENG, true, true);
+	char** myHash = randomHash(LENG, SYMB, DEBU);
 
 	// TODO: fix this fu#king print
 	for (int i = 0; i < LENG; i++) {
